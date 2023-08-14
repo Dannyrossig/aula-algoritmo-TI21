10241103864a82ecbd9fa8c7ee02f39b35221b70
Dannyrossig <141831795+Dannyrossig@users.noreply.github.com>
2023-08-14 11:07:17 -0300

    desafio 3

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius, fahrenheit;
+            Console.WriteLine(" digite a temperatura em celsius: ");
+            celsius = float.Parse(Console.ReadLine());
+
+            fahrenheit = celsius * 9 / 5 + 32;
+
+            Console.WriteLine(" A temperatura em Fahrenheit é: " + fahrenheit);
+
+            Console.ReadKey();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
