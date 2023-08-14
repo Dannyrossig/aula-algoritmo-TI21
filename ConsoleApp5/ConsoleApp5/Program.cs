2b5891f2daaed18d59acff84c42a394613237c98
Dannyrossig <141831795+Dannyrossig@users.noreply.github.com>
2023-08-14 11:52:20 -0300

    desafio 5

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float pagamento, valor, troco;
+
+            Console.WriteLine("digite o valor total da compra: ");
+            valor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o valor pelo Cliente: ");
+            pagamento = float.Parse(Console.ReadLine());
+
+            troco = pagamento - valor;
+
+            Console.WriteLine("o valor do seu troco é: " + troco);
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
