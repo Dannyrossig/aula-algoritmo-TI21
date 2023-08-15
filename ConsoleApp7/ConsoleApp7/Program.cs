596c43961c5c09a016670c8c412c3a507e4e7bc6
Dannyrossig <141831795+Dannyrossig@users.noreply.github.com>
2023-08-15 11:13:00 -0300

    exercício 2

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Qual idade vc tem?");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >=  18)
+            {
+                Console.WriteLine(" de maior");
+
+            }
+                
+                else
+            {
+                Console.WriteLine(" de menor");
+
+            }
+           
+
+
+
+
+
+
+
+                Console.ReadKey();  
+
+        }
+    }
+}
