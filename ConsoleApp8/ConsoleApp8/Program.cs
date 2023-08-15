d941852d4ffb91599e52cc28e9797c737e585b81
Dannyrossig <141831795+Dannyrossig@users.noreply.github.com>
2023-08-15 11:54:07 -0300

    exercício 4

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, altura;
+            float imc;
+
+            Console.WriteLine(" qual peso");
+            peso = float.Parse(Console.ReadLine());
+            if (peso <= 18.5)
+            {
+
+            }
+
+            Console.WriteLine(" qual altura");
+            altura = float.Parse(Console.ReadLine());
+            if (altura >= 1.60)
+            {
+
+            }
+
+            Console.WriteLine(" qual imc");
+            imc = float.Parse(Console.ReadLine());
+            if 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
